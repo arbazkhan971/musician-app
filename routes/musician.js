5da2e81ad33e9611ce2b1def87090731bc8aba26
arbaz khan <arbazkhan971@gmail.com>
2020-11-06 17:15:46 +0530

    name route is added

--- routes/musician.js
+++ routes/musician.js
@@ -9,6 +9,12 @@ router.get('/health', (req, res) => {
   res.status('200').send("Status: ok!");
 });
 
+// healthcheck
+router.get('/name', (req, res) => {
+  res.status('200').send("Status: name:arbaz!");
+});
+
+
 // retrieve all musicians from data store
 router.get('/all', (req, res) => {
   const { musician } = req.app.locals;
